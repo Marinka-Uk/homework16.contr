3c64d54a50f7b75aa11eb1f3cd6964eb68ef7df3
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-09-11 18:57:52 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import fetchCountries from './fetchCountries'
+import debounce from 'lodash.debounce'
 const searchQuery = document.querySelector('.search')
 
 
-searchQuery.addEventListener('input', () => {
-    fetchCountries(searchQuery.value).then(console.log)
-})
+searchQuery.addEventListener('input', debounce( () => {
+    fetchCountries(searchQuery.value).then(countries => {
+        if (countries.length === 1) {
+            createCounryCard(countries)
+        } else if(countries.length > 1 && countries.length >= 10){
+            createCounryList(countries)
+        }
+    }
+     )
+}, 3000))
+
+function createCounryList(counryList) {
+    return counryList.map((country) => {
+        return  `<li>
+    <p>
+${country.name.official}
+</p>
+</li>
+    `
+    }) 
+
+    
+}
+function createCounryCard(country) {
+    return `<h1></h1>
+<p>Capital: ${}</p>
+<p>Population: ${}</p>
+<p>Languages: ${}</p>
+<ul></ul>
+<img src="${}">
+`
+
+}
+
+function createNotification() {
+    
+}
