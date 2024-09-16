f18627ab8bb272e46db0cb65eda9e4ca6dd8db7b
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-09-16 22:12:06 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import fetchCountries from './fetchCountries'
 import debounce from 'lodash.debounce'
+import { alert } from 'pnotify'
 const searchQuery = document.querySelector('.search')
 
+const resultsContainer = document.querySelector('.result')
 
 searchQuery.addEventListener('input', debounce( () => {
     fetchCountries(searchQuery.value).then(countries => {
@@ -15,28 +17,27 @@ searchQuery.addEventListener('input', debounce( () => {
 }, 3000))
 
 function createCounryList(counryList) {
-    return counryList.map((country) => {
-        return  `<li>
-    <p>
-${country.name.official}
-</p>
-</li>
-    `
-    }) 
-
-    
+    const listMarkup = counryList.map(country => {
+        return `<li>${country.name.official}</li>`
+    }).join('')
+resultsContainer.innerHTML = `<ul>${listMarkup}</ul>`
 }
+
 function createCounryCard(country) {
-    return `<h1></h1>
-<p>Capital: ${}</p>
-<p>Population: ${}</p>
-<p>Languages: ${}</p>
+    return `<h1>Contries search</h1>
+<p>Capital: ${country.capital}</p>
+<p>Population: ${country.population}</p>
+<p>Languages: ${country.languages.map(lang => lang.name).join(', ')}</p>
 <ul></ul>
-<img src="${}">
+<img src="${country.flag}" alt="Flag of ${country.name}">
 `
-
+resultsContainer.innerHTML = cardMarkup
 }
 
-function createNotification() {
-    
+function createNotification(message) {
+    alert({
+        text: message,
+            type: 'info',
+        delay: 2000,
+    })
 }
